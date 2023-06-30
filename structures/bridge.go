40776419b32628db0e9a33c35933bbd4d498b53e
aeryzhao <iszhaoxg@outlook.com>
2023-06-30 18:09:39 +0800

    :rotating_light: fix

--- structures/bridge.go
+++ structures/bridge.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-type Computer interface {
+type computer interface {
 	Print()
 	SetPrinter(printer Printer)
 }
@@ -11,29 +11,29 @@ type Printer interface {
 	PrintFile()
 }
 
-type Mac struct {
+type mac struct {
 	printer Printer
 }
 
-func (m *Mac) Print() {
+func (m *mac) Print() {
 	fmt.Println("Print request for mac")
 	m.printer.PrintFile()
 }
 
-func (m *Mac) SetPrinter(p Printer) {
+func (m *mac) SetPrinter(p Printer) {
 	m.printer = p
 }
 
-type Windows struct {
+type windows struct {
 	printer Printer
 }
 
-func (w *Windows) Print() {
+func (w *windows) Print() {
 	fmt.Println("Print request for windows")
 	w.printer.PrintFile()
 }
 
-func (w *Windows) SetPrinter(p Printer) {
+func (w *windows) SetPrinter(p Printer) {
 	w.printer = p
 }
 
@@ -53,14 +53,14 @@ func main() {
 	hp := &HP{}
 	epson := &Epson{}
 
-	mac := &Mac{}
+	mac := &mac{}
 	mac.SetPrinter(hp)
 	mac.Print()
 
 	mac.SetPrinter(epson)
 	mac.Print()
 
-	win := &Windows{}
+	win := &windows{}
 	win.SetPrinter(hp)
 	win.Print()
 
